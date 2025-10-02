916715b4272a509d10c2f927875ef8913f0ec0f2
José Gois <ze.gois.00@gmail.com>
2025-10-02 16:33:31 -0300

    ours

--- pkg/rust_template_wasm_bindgen.js
+++ pkg/rust_template_wasm_bindgen.js
@@ -209,7 +209,7 @@ async function __wbg_init(module_or_path) {
     }
 
     if (typeof module_or_path === 'undefined') {
-        module_or_path = new URL('rust_template_wasm_bg.wasm', import.meta.url);
+        module_or_path = new URL('rust_template_wasm_bindgen_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn greet(name: &str) -> String {
-    format!("Olá, {}!", name)
+    format!("Hello, {}!", name)
 }
